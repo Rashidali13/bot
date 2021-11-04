71fa96b03f1bdc95b060bbc62f130145b9476f49
unknown <codefaculty3@gmail.com>
2021-11-04 14:12:15 +0500

    new files and folders added

---
+++ run.py
@@ -0,0 +1,7 @@
+from booking.booking import Booking
+
+with Booking() as bot:
+    bot.land_first_page()
+    bot.change_currency(currency='USD')
+    bot.select_place_to_go('New York')
+    bot.select_dates(check_in_date='2021-10-29', check_out_date='2021-11-01')
